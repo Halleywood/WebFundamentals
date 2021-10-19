26b82a41f1972076272a1caff3920b992a9bde91
Kelsey Halley <87944545+Halleywood@users.noreply.github.com>
2021-10-18 20:56:19 -0700

    practice with hover over video play/pause function

---
+++ Halley_Kelsey_VideoPreview.js
@@ -0,0 +1,11 @@
+
+var vid=document.querySelector("#myvideo");
+function playVid(element){
+    element.muted = true;
+    element.play();
+    
+}
+function pauseVid(element){
+    element.muted = true;
+    element.pause();
+}
